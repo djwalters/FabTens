4b3789d26c12ec0a6e20d108f67cf1be06f71e1f
David Walters <walters.david.j@gmail.com>
2015-03-17 17:01:25 -0600

    Updated plot saving script to set figure size and improve performance.

--- SaveAllPlots.m
+++ SaveAllPlots.m
@@ -1,13 +1,24 @@
+% Script saves all open figure windows to 3 file formats to the directory
+% specified by the user.  (3 formats: .fig, .emf, .png)
 hfigs = get(0,'children');  %Get list of figures
 
 LocalPath = 'C:\Doctoral Research\Mechanical Testing\Radiation Recrystallization\Fabric Tensor and ANSYS\Matlab 3D Segmentation Results\';
 directory = uigetdir(LocalPath,...
             'Select  directory to save plots');
 
-for i = 1:length(hfigs)    
-    figure(hfigs(i))        %Bring figure to foreground
+for i = 1:length(hfigs)
+    % Set figure size and position on screen
+    set(i,'Units','inches')
+    set(i,'Position',[3,3,6,3.5])
+    figure(i)        %Bring figure to foreground
     filename = get(gcf,'name'); %Get window title for filename
+    % If no special window title specified, give generic filename
+    if isempty(filename)
+        fname = sprintf('Figure %2.0f',i);
+        filename = fname;
+    end
     FilePath = fullfile(directory,filename);
+    % Save figures to specified path
     saveas(hfigs(i),FilePath,'fig');
     saveas(hfigs(i),FilePath,'emf');
     saveas(hfigs(i),FilePath,'png');
